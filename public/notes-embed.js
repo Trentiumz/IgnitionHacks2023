24c6011e84d405a1455f5cbc45147b707693bcfd
Trentiumz <75898115+Trentiumz@users.noreply.github.com>
2023-08-26 14:27:52 +0000

    Stylized textbox

--- public/notes-embed.js
+++ public/notes-embed.js
@@ -1,36 +1,10 @@
 window.onload = () => {
-  // load in the list of pages that a user can select
-  axios.get('/get-page-list').then((res) => {
-    if(res.status === 200){
-      // parse data for only title and id
-      const data = res.data.results
-      const parsedData = data.map((x) => {
-        const titles = x.properties.title.title
-        return {
-          id: x.id,
-          title: titles.length > 0 ? titles[0].plain_text : ""
-        }
-      })
-
-      // add page options to the form
-      parsedData.forEach((x) => {
-        const option = document.createElement('option')
-        option.innerHTML = x.title
-        option.setAttribute('value', x.id)
-        document.getElementById('notes').appendChild(option)
-      })
-    } else {
-      document.getElementById('message').innerHTML = "An error was made fetching pages"
-      console.log(res)
-    }
-  })
-
   // handle form submission
   document.getElementById('generation-form').addEventListener('submit', (e) => {
     e.preventDefault()
     const formData = new FormData(e.target)
     const data = {
-      notes_id: formData.get('notes')
+      notes_id: formData.get('notes').split('-').slice(-1)[0]
     }
     console.log(data)
   })
