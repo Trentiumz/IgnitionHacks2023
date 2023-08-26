194d61b39a21e0b6d0976e5130f159cff8e12ffa
Trentiumz <75898115+Trentiumz@users.noreply.github.com>
2023-08-26 03:19:31 +0000

    Create Embed

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 const express = require('express')
+require('dotenv').config()
 const app = express()
 const port = 3000
+
+const {Client} = require("@notionhq/client")
+const notion = new Client({auth: process.env.NOTION_KEY})
+
+app.use(express.static('public'))
+
+app.get('/notes_embed', (req, res) => {
+  res.sendFile(__dirname + "/static/notes-embed.html")
+})
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
