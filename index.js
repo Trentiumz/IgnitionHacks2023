4a8e2180cf9feb80a1a10e20cf690aa606d3f832
Trentiumz <75898115+Trentiumz@users.noreply.github.com>
2023-08-26 03:48:17 +0000

    Modify notion directly

--- index.js
+++ index.js
@@ -6,6 +6,14 @@ const port = 3000
 const {Client} = require("@notionhq/client")
 const notion = new Client({auth: process.env.NOTION_KEY})
 
+const get_content = async () => {
+  const blockId = process.env.NOTION_PAGE_ID
+  const response = await notion.blocks.children.list({
+    block_id: blockId
+  })
+  return response
+}
+
 app.use(express.static('public'))
 
 app.get('/notes_embed', (req, res) => {
@@ -14,6 +22,36 @@ app.get('/notes_embed', (req, res) => {
 
 app.get('/', (req, res) => {
     res.send('Hello World!')
+})
+
+app.post('/read-page-content', async (req, res) => {
+  const response = await get_content()
+  res.json(response)
+})
+
+app.post('/add-sample-content', async (req, res) => {
+  const blockId = process.env.NOTION_PAGE_ID
+  const response = await notion.blocks.children.append({
+    block_id: blockId,
+    children: [
+      {
+        object: 'block',
+        type: 'paragraph',
+        paragraph: {
+          rich_text: [
+            {
+              type: 'text',
+              text: {
+                content: 'Sample Text!'
+              },
+              plain_text: 'text'
+            }
+          ]
+        }
+      }
+    ]
+  })
+  res.json(response)
 })
 
 app.listen(port, () => {
