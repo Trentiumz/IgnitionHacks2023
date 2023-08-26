b6ddd60c4b0d46956c5cbf019b6bd754d1f7eec5
Max Sun <maxsun715@gmail.com>
2023-08-26 18:40:44 -0400

    update extension

--- extension/background.js
+++ extension/background.js
@@ -1,20 +1,60 @@
 let url = window.location.href;
-let id = url.substring(url.lastIndexOf('-') == -1 ? url.lastIndexOf('/') + 1 : url.lastIndexOf('-') + 1, id.indexOf('?') == -1 ? url.length : url.indexOf('?'));
+let id = url.substring(
+  url.lastIndexOf("-") == -1
+    ? url.lastIndexOf("/") + 1
+    : url.lastIndexOf("-") + 1,
+  url.indexOf("?") == -1 ? url.length : url.indexOf("?")
+);
 
-if(document.querySelector("div[id='notion-app']")) {
-    var button = document.createElement("button");
-    button.innerHTML = "Gnostic";
-    document.getElementById("notion-topbar-action-buttons").appendChild(button);
-}
-
-button.addEventListener ("click", async function() {
-    await fetch('https://ignitionhacks2023.danielye6.repl.co/generatequiz', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            "id": id
-        })
+const render = () => {
+  var topbar = document.getElementsByClassName("notion-topbar-action-buttons").item(0);
+  var lightMode = document.getElementsByClassName("notion-cursor-listener").item(0).style.backgroundColor === "white";
+  var button = document.createElement("button");
+  button.className = "gnostic-button";
+  button.innerHTML = `<img src="${chrome.runtime.getURL(lightMode ? "logo_black.png" : "logo.png")}" width="120" height="30">`;
+  button.style.position = "relative";
+  button.style.backgroundColor = "transparent";
+  button.style.border = "none";
+  button.style.marginRight = "10px";
+  button.style.marginTop = "4px";
+  topbar.prepend(button);
+  button.addEventListener("click", async function () {
+    button.innerHTML = `<img src="${chrome.runtime.getURL(lightMode ? "loading_black.gif" : "loading.gif")}" width="120" height="80">`;
+    button.style.marginTop = "4px";
+    console.log(id);
+    await fetch(`http://localhost:3000/generatequiz/${id}`, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }).then((response) => {
+        console.log(response);
+        button.innerHTML = `<img src="${chrome.runtime.getURL(lightMode ? "logo_black.png" : "logo.png")}" width="120" height="30">`;
+        button.style.marginTop = "4px";
+        let popup = document.createElement("div").innerHTML = response.status === 200 ? "Successfully generated quiz" : "Failed to generate quiz";
+        popup.style.position = "absolute";
+        popup.style.alignBottom = "50px";
+        popup.style.alignRight = window.innerWidth/2;
+        popup.style.backgroundColor = "#5891ed";
+        popup.style.padding = "10px";
+        popup.style.borderRadius = "10px";
+        popup.style.color = "white";
+        popup.style.fontFamily = "Helvetica";
+        popup.style.fontSize = "16px";
+        popup.style.fontWeight = "bold";
+        setTimeout(function() {
+            popup.remove();
+        }, 3000);
     });
-});
+  });
+};
+
+const wait = () => {
+  if (document.getElementsByClassName("notion-topbar-action-buttons").item(0)) {
+    render();
+  } else {
+    setTimeout(wait, 200);
+  }
+};
+
+wait();
