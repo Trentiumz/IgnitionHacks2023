1693b1d1cf2722d7a41923f32a6df05392e730f5
Max Sun <maxsun715@gmail.com>
2023-08-26 09:13:12 -0400

    add openai route

---
+++ route.js
@@ -0,0 +1,27 @@
+import OpenAI from 'openai';
+import { OpenAIStream, StreamingTextResponse } from 'ai';
+ 
+// Create an OpenAI API client (that's edge friendly!)
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+ 
+// Set the runtime to edge for best performance
+export const runtime = 'edge';
+ 
+export async function POST(req) {
+  const { prompt } = await req.json();
+  
+  // Ask OpenAI for a streaming completion given the prompt
+  const response = await openai.completions.create({
+    model: 'text-davinci-003',
+    stream: true,
+    temperature: 0.6,
+    max_tokens: 300,
+    prompt: `For every statement below, generate a question that will test if I read the statement. \n${prompt}`,
+  });
+  // Convert the response into a friendly text-stream
+  const stream = OpenAIStream(response);
+  // Respond with the stream
+  return new StreamingTextResponse(stream);
+}
