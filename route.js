06424e75f3205638947508a24839a3118368e857
Max Sun <maxsun715@gmail.com>
2023-08-26 09:22:50 -0400

    update prompt

--- route.js
+++ route.js
@@ -9,16 +9,14 @@ const openai = new OpenAI({
 // Set the runtime to edge for best performance
 export const runtime = 'edge';
  
-export async function POST(req) {
-  const { prompt } = await req.json();
-  
+export async function POST(prompt) {  
   // Ask OpenAI for a streaming completion given the prompt
   const response = await openai.completions.create({
     model: 'text-davinci-003',
     stream: true,
     temperature: 0.6,
     max_tokens: 300,
-    prompt: `For every statement below, generate a question that will test if I read the statement. \n${prompt}`,
+    prompt: `${prompt}`,
   });
   // Convert the response into a friendly text-stream
   const stream = OpenAIStream(response);
